8bb23b4281404cd633d0b61154bd2fe57121d95f
Parker Illig <pillig@fanatics.com>
2019-08-27 11:04:56 -0600

    more exercises

--- exercises/lib/exercises/advanced.ex
+++ exercises/lib/exercises/advanced.ex
@@ -2,12 +2,17 @@ defmodule Exercises.Advanced do
   @doc """
   Return the average value of all numbers in the list.
   """
+
   def average([head | tail]) do
     do_average(tail, head, 1)
   end
 
-  defp do_average(_list, _sum, _count) do
-    raise "not implemented yet"
+  defp do_average([], sum, count) do
+    div(sum, count)
+  end
+
+  defp do_average([head | tail], sum, count) do
+    do_average(tail, head + sum, count + 1)
   end
 
   @doc """
@@ -17,20 +22,28 @@ defmodule Exercises.Advanced do
     do_odds_and_evens(list, [], [])
   end
 
-  defp do_odds_and_evens([head, tail], odds, evens) do
-    (head % 2) == 0 -> do_odds_and_evens(tail, odds, [head | evens])
-    (head % 2) == 1 -> do_odds_and_evens(tail, [head | odds], evens)
-  end
-
   defp do_odds_and_evens([], odds, evens) do
     {odds, evens}
+  end
+
+  defp do_odds_and_evens([head | tail], odds, evens) do
+    case rem(head, 2) do
+      0 -> do_odds_and_evens(tail, odds, evens ++ [head])
+      1 -> do_odds_and_evens(tail, odds ++ [head], evens)
+    end
   end
 
   @doc """
   Returns the three largest numbers of the list
   """
-  def three_largest(_list) do
-    raise "not implemented yet"
+  def three_largest(list) do
+    # Enum.sort(list, fn left, right -> left >= right end)
+    list
+    # |> Enum.sort(&(&1 >= &2)))
+    # |> Enum.take(3)
+    # |> Enum.sort()
+    |> Enum.sort()
+    |> Enum.take(-3)
   end
 
   @doc """
@@ -38,7 +51,14 @@ defmodule Exercises.Advanced do
 
   TIP: Use the functions `String.split/2`, `String.capitalize/1`, and `Enum.join/1`.
   """
-  def capitalize_all(_string) do
-    raise "not implemented yet"
+  defp capitalize_all_list(input_list) do
+    case input_list do
+      [head] -> [String.capitalize(head)]
+      [head, tail] -> [String.capitalize(head)] ++ capitalize_all_list(tail)
+    end
+  end
+
+  def capitalize_all(input_string) do
+    Enum.join(capitalize_all_list(String.split(input_string, " ")), " ")
   end
 end
--- exercises/test/exercises/advanced_test.exs
+++ exercises/test/exercises/advanced_test.exs
@@ -2,14 +2,12 @@ defmodule Exercises.AdvancedTest do
   use ExUnit.Case
   alias Exercises.Advanced
 
-  @tag :skip
   test "average/1" do
     assert Advanced.average([5]) == 5
     assert Advanced.average([5, 5]) == 5
     assert Advanced.average([5, 6, 7]) == 6
   end
 
-  @tag :skip
   test "odds_and_evens/1" do
     assert Advanced.odds_and_evens([]) == {[], []}
     assert Advanced.odds_and_evens([1, 3, 5]) == {[1, 3, 5], []}
@@ -17,7 +15,6 @@ defmodule Exercises.AdvancedTest do
     assert Advanced.odds_and_evens([6, 5, 4, 3, 2, 1]) == {[5, 3, 1], [6, 4, 2]}
   end
 
-  @tag :skip
   test "three_largest/1" do
     assert Advanced.three_largest([]) == []
     assert Advanced.three_largest([4, 9, 6, 3, 1]) == [4, 6, 9]
